da0bf76c44b889142777b1d2c1363ed950a137c4
SimonArtz <artz0003@algonquinlive.com>
2022-02-13 00:07:29 -0500

    Step 1

--- app/src/main/java/com/cst2355/artz/MainActivity.java
+++ app/src/main/java/com/cst2355/artz/MainActivity.java
@@ -3,12 +3,25 @@ package com.cst2355.artz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.view.View;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+    }
+
+    public void toastMSG(String str) {
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_LONG).show();
+    }
+
+    public void toastView(View view) {
+        String str = MainActivity.this.getResources().getString(R.string.toastText);
+        this.toastMSG(str);
     }
 }
---
+++ app/src/main/java/com/cst2355/artz/snackbarUndo.java
@@ -0,0 +1,11 @@
+package com.cst2355.artz;
+
+import android.view.View;
+
+public class snackbarUndo implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+
+    }
+    //unable to implement snackbar
+}
