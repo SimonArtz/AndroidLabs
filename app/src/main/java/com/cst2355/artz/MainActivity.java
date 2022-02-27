89a677cb633fefecd1cace052ded28e20f598563
SimonArtz <artz0003@algonquinlive.com>
2022-02-27 00:01:57 -0500

    Lab 3

--- app/src/main/java/com/cst2355/artz/MainActivity.java
+++ app/src/main/java/com/cst2355/artz/MainActivity.java
@@ -2,7 +2,11 @@ package com.cst2355.artz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.CompoundButton;
 import android.view.View;
@@ -10,10 +14,25 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText em, pass;
+    Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_login);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Context context = MainActivity.this;
+        SharedPreferences sharedPref = context.getSharedPreferences("emailFile", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        em = (EditText) findViewById(R.id.emailText);
+        String emT = em.getText().toString();
+        editor.putString("Email", emT);
+        editor.commit();
     }
 
     public void toastMSG(String str) {
